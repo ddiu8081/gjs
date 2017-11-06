1ebb78dd22b505bd2ae161c1b3eea9a6ec00a29d
ddiu8081 <ddiu8081@gmail.com>
2017-11-06 23:41:08 +0800

    1106 - fix bugs on the day have no gidden messages

--- assets/js/gjs.js
+++ assets/js/gjs.js
@@ -13,7 +13,6 @@ var initPage = function (date) {
 
     $.getJSON("api.php?f=getMessege&date=" + date, function (data) {
         console.log(data);
-        $('#hide_div').show("slow");
 
         if (data.color.length != 0) {
             $("body").css("background-color", data.color);
@@ -51,26 +50,37 @@ var initPage = function (date) {
             $('#message_pic').hide("slow");
         }
 
-        if (data.hidden_text) {
-            $('#hidden_text').show("slow");
-            $('#hidden_text').text(data.hidden_text);
-        } else {
-            $('#hidden_text').hide("slow");
-        }
+        if (data.hidden_text || data.hidden_pic || data.hidden_a_href) {
+            console.log('show!');
+            $('#hide_div').show("slow");
+            if (data.hidden_text) {
+                $('#hidden_text').show();
+                $('#hidden_text').text(data.hidden_text);
+            } else {
+                $('#hidden_text').hide();
+            }
 
-        if (data.hidden_pic) {
-            $('#hidden_pic').show("slow");
-            $('#hidden_pic').attr('src', data.hidden_pic);
-        } else {
-            $('#hidden_pic').hide("slow");
-        }
+            if (data.hidden_pic) {
+                $('#hidden_pic').show();
+                $('#hidden_pic').attr('src', data.hidden_pic);
+            } else {
+                $('#hidden_pic').hide();
+            }
 
-        if (data.hidden_a_href) {
-            $('#hidden_a').show("slow");
-            $('#hidden_a').text(data.hidden_a_text);
-            $('#hidden_a').attr('href', data.hidden_a_href);
-        } else {
-            $('#hidden_a').hide("slow");
+            if (data.hidden_a_href) {
+                $('#hidden_a').show();
+                $('#hidden_a').text(data.hidden_a_text);
+                $('#hidden_a').attr('href', data.hidden_a_href);
+            } else {
+                $('#hidden_a').hide();
+            }
+        }
+        else {
+            $('#hidden_text').hide();
+            $('#hidden_pic').hide();
+            $('#hidden_a').hide();
+            $('#hide_div').hide();
+            console.log('hide!');
         }
 
         if (data.like == '0') {
@@ -95,7 +105,6 @@ var likeMessege = function (date) {
 
 var playMusic = function (musicID) {
     var musicUrl = 'http://music.163.com/api/132434.mp3';
-    var musicUrl = 'http://'
 }
 
 var c171019 = function () {
@@ -107,7 +116,7 @@ var c171019 = function () {
         var x = e.pageX,
             y = e.pageY;
         $i.css({
-            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "z-index": 99999999,
             "top": y - 20,
             "left": x,
             "position": "absolute",
@@ -134,13 +143,16 @@ var c171031 = function () {
         $(".hide").show("fast");
         setTimeout(function() {
             $(".hide").hide("fast");
-        }, 1000);
+        }, 600);
 
     });
     $("body").css(
         "background","url(./001.gif)"
     );
-    $("body").click(function (e) {
+}
+
+var c171717 = function () {
+    $("#music").click(function() {
 
     });
 }
