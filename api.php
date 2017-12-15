e54f4e4cda0fe01e5a6a1dec955d39702377eff0
ddiu8081 <ddiu8081@gmail.com>
2017-12-15 13:19:10 +0800

    git re-init

--- api.php
+++ api.php
@@ -4,11 +4,11 @@ $fun = $_GET["f"];
 $date = $_GET["date"];
 
 if ($fun == "getMessege") {
-    $file_contents = file_get_contents('https://api.ddiu.site/gjs/get/'.$date);
+    $file_contents = file_get_contents('https://api.ddiu.site/gjs/get-e/'.$date);
     echo $file_contents;
 }
 if ($fun == "like") {
-    $file_contents = file_get_contents('https://api.ddiu.site/gjs/like/'.$date);
+    $file_contents = file_get_contents('https://api.ddiu.site/gjs/like-e/'.$date);
     echo $file_contents;
 }
 
